9df05edd10d52dfa11b07de92a90a85178c3ed67
aryaintarann <aryaintaran09@proton.me>
2026-03-26 22:46:31 +0800

    style: remove unused import in main_shell.dart

--- lib/screens/main_shell.dart
+++ lib/screens/main_shell.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import '../config/app_theme.dart';
 
 class MainShell extends StatelessWidget {
   final Widget child;
